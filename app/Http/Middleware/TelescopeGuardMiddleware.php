02fe377ce81ce257f07c8309942a5b40cbc50223
Sergey <nautbek@gmail.com>
2026-09-10 14:33:23 +0200

    Закрываем телескоп от внешнего мира.

---
+++ app/Http/Middleware/TelescopeGuardMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use function Laravel\Prompts\error;
+
+class TelescopeGuardMiddleware
+{
+    private const array ACCESS_IPS = [
+        '188.212.124.75'
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!in_array($request->getClientIp(), self::ACCESS_IPS)) {
+            return new Response('Only for admin access.', Response::HTTP_FORBIDDEN);
+        }
+        return $next($request);
+    }
+}
--- config/telescope.php
+++ config/telescope.php
@@ -1,6 +1,5 @@
 <?php
 
-use Laravel\Telescope\Http\Middleware\Authorize;
 use Laravel\Telescope\Watchers;
 
 return [
@@ -93,8 +92,7 @@ return [
     */
 
     'middleware' => [
-        'web',
-        Authorize::class,
+        \App\Http\Middleware\TelescopeGuardMiddleware::class,
     ],
 
     /*
